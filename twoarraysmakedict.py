4b54042304d9706aa5395f315b1961e48274dddb
Hana-Luong <56649387+Hana-Luong@users.noreply.github.com>
2021-02-07 20:25:27 -0800

    Add files via upload

---
+++ twoarraysmakedict.py
@@ -0,0 +1,12 @@
+# Zip Arrays into Map
+
+# Given two arrays, create an associative array (aka hash map, an obj / dictionary) 
+# containing keys from the first array, and values from the second.
+# Associative arrays are sometimes called maps because a key (string) maps to a value 
+
+# Input: ["abc", 3, "yo"], [42, "wassup", True]
+# Output: {
+#     "abc": 42,
+#     3: "wassup",
+#     "yo": True,
+# }
