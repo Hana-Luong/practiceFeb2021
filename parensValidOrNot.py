6e0b833cb9195ac0e21e252d344844d8786a6c6f
Hana-Luong <56649387+Hana-Luong@users.noreply.github.com>
2021-02-16 08:00:00 -0800

    Add files via upload

---
+++ parensValidOrNot.py
@@ -0,0 +1,27 @@
+# Parens Valid
+# Given a string that has parenthesis in it
+# return whether the parenthesis are valid
+
+# Input: "Y(3(p)p(3)r)s"
+# Output: True
+
+# Input: "N(0(p)3"
+# Output: False
+
+def parens_valid(stringy):
+    pairs =0
+    for char in stringy:
+        if char == "(":
+            pairs +=1
+            print(pairs)
+        if char == ")":
+            pairs -=1
+            print(pairs)
+        if pairs < 0:
+            return False
+    if pairs == 0:
+        return True
+    else:
+        return False
+        
+print(parens_valid("y(3(p)p(3)r)0s"))
