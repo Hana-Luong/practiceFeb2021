de4c404130c90f88c814b8a0b9b3f47203ee29c0
Hana-Luong <56649387+Hana-Luong@users.noreply.github.com>
2021-02-21 21:10:39 -0800

    Add files via upload

---
+++ bracesValid.py
@@ -0,0 +1,13 @@
+ * takes a string
+ * returns a boolean
+ * are the braces valid
+ */
+
+function bracesValid(str) {
+  // your code here
+}
+
+console.log(bracesValid('{[()]}')); // should log true
+console.log(bracesValid('{[()}]')); // should log false
+console.log(bracesValid('{[a()b]c}')); // should log true
+console.log(bracesValid('{}[]()[(])')); // should log false
